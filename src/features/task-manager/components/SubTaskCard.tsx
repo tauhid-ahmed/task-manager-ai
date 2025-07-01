5e5f784572e13acc31e05daf3dfc7a98564acece
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-07-01 23:52:07 +0600

    Add SubTaskCard component to display AI suggested subtasks

---
+++ src/features/task-manager/components/SubTaskCard.tsx
@@ -0,0 +1,44 @@
+import {
+  LucideSparkles,
+  LucideSquare,
+  LucideSquareCheckBig,
+} from "lucide-react";
+import { type SubTask } from "../types/task-manager.types";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+type Props = {
+  subTasks?: SubTask[];
+} & React.ComponentProps<"div">;
+
+export default function SubTaskCard({ subTasks }: Props) {
+  if (!subTasks || subTasks.length <= 0) return null;
+  return (
+    <div className="bg-violet-50/50 p-4 mt-10 border border-violet-200/50 rounded space-y-4">
+      <h3 className="flex items-center text-violet-500 font-semibold gap-2">
+        <LucideSparkles /> AI Suggested Subtasks
+      </h3>
+      <ul className="space-y-1">
+        {subTasks.map((subTask) => (
+          <li className={cn("flex items-center text-sm")} key={subTask.id}>
+            <Button size="icon" variant="ghost">
+              {subTask.status === "pending" ? (
+                <LucideSquare size={16} />
+              ) : (
+                <LucideSquareCheckBig className="text-violet-500" size={16} />
+              )}
+            </Button>
+            <span
+              className={cn(
+                "font-medium text-foreground/80",
+                subTask.status === "completed" && "line-through"
+              )}
+            >
+              {subTask.title}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
