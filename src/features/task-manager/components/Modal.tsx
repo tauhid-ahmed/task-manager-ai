f45eea3cec43eb8c2ff408be855465ad88798d5d
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-07-01 20:20:49 +0600

    Add Modal component for task management with form and delete confirmation

---
+++ src/features/task-manager/components/Modal.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
+import TaskManagerForm from "./TaskManagerForm";
+import { useTaskManager } from "../provider";
+import TaskDeleteConfirm from "./TaskDeleteConfirm";
+
+export default function Modal() {
+  const { state, handleCloseModal } = useTaskManager();
+  const { status } = state;
+  const openModal =
+    status === "creating" || status === "editing" || status === "deleting";
+
+  return (
+    <Dialog open={openModal} onOpenChange={handleCloseModal}>
+      <DialogContent>
+        <DialogTitle className="absolute -left-[9999px] h-px overflow-hidden">
+          Task Manager Modal
+        </DialogTitle>
+        <TaskManagerForm />
+        <TaskDeleteConfirm />
+      </DialogContent>
+    </Dialog>
+  );
+}
