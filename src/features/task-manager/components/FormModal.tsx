4ef07524eea4fa87b6b617463bf0bd64c5ca45f2
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-07-01 20:43:24 +0600

    Add FormModal component for task management with dynamic dialog handling

--- src/features/task-manager/components/FormModal.tsx
+++ src/features/task-manager/components/FormModal.tsx
@@ -2,13 +2,11 @@
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import TaskManagerForm from "./TaskManagerForm";
 import { useTaskManager } from "../provider";
-import TaskDeleteConfirm from "./TaskDeleteConfirm";
 
 export default function Modal() {
   const { state, handleCloseModal } = useTaskManager();
   const { status } = state;
-  const openModal =
-    status === "creating" || status === "editing" || status === "deleting";
+  const openModal = status === "creating" || status === "editing";
 
   return (
     <Dialog open={openModal} onOpenChange={handleCloseModal}>
@@ -17,7 +15,6 @@ export default function Modal() {
           Task Manager Modal
         </DialogTitle>
         <TaskManagerForm />
-        <TaskDeleteConfirm />
       </DialogContent>
     </Dialog>
   );
