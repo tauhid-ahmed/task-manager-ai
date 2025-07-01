4f1862f6562736bef809222efcaa4cfbf911efb0
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-07-01 20:44:12 +0600

    Replace TaskDeleteConfirm component with TaskDeleteModal for improved task deletion handling

--- src/features/task-manager/components/TaskDeleteConfirm.tsx
+++
@@ -1,7 +0,0 @@
-import { useTaskManager } from "../provider";
-
-export default function TaskDeleteConfirm() {
-  const { state } = useTaskManager();
-  if (state.status !== "deleting") return null;
-  return <div>TaskDeleteConfirm</div>;
-}
